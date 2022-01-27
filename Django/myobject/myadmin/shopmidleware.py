3793a06419074fa441c133f6a5e43ac7937dc398
myrondong <1052707337@qq.com>
2022-01-27 18:18:51 +0800

    Django

--- Django/myobject/myadmin/shopmidleware.py
+++ Django/myobject/myadmin/shopmidleware.py
@@ -15,31 +15,16 @@ class ShopMiddleware:
 
         # 判断管理后台是否登录
         # 定义后台不登录也可直接访问的url列表
-        urllist = ['/myadmin/login', '/myadmin/logout', '/myadmin/dologin', '/myadmin/verify']
+        ulist = ['/myadmin/login','/myadmin/logout','/myadmin/dologin']
         # 判断当前请求url地址是否是以/myadmin开头,并且不在urllist中，才做是否登录判断
-        if re.match(r'^/myadmin', path) and (path not in urllist):
-            # 判断是否登录(在于session中没有adminuser)
+        if re.match(r'^/myadmin', path) and path not in ulist:
+            print(request.session)
+            # 判断登录（key 名为adminuser是否存在session中）
             if 'adminuser' not in request.session:
-                # 重定向到登录页
-                return redirect(reverse("myadmin_login"))
+
+                return redirect(reverse("myadmin_login_index"))
                 # pass
 
-        # 判断大堂点餐请求的判断，判断是否登录（session中是否有webuser）
-        if re.match(r'^/web', path):
-            # 判断是否登录(在于session中没有webuser)
-            if 'webuser' not in request.session:
-                # 重定向到登录页
-                return redirect(reverse("web_login"))
-
-        # 判断移动端是否登录
-        # 定义移动端不登录也可直接访问的url列表
-        urllist = ['/mobile/register', '/mobile/doregister']
-        # 判断当前请求url地址是否是以/mobile开头,并且不在urllist中，才做是否登录判断
-        if re.match(r'^/mobile', path) and (path not in urllist):
-            # 判断是否登录(在于session中没有mobileuser)
-            if 'mobileuser' not in request.session:
-                # 重定向到登录页
-                return redirect(reverse("mobile_register"))
 
         response = self.get_response(request)
         # Code to be executed for each request/response after
--- Django/myobject/myadmin/views/index.py
+++ Django/myobject/myadmin/views/index.py
@@ -1,6 +1,9 @@
+import hashlib
+import random
+
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from myadmin.models import User
 
 # Create your views here.
 # 后台管理首页
@@ -15,8 +18,22 @@ def login(request):
 
 # 执行管理员登录
 def dologin(request):
-    pass
-
+    try:
+        user = User.objects.get(username=request.POST['username'])
+        # 判断是否管理员
+        md5 = hashlib.md5()
+        s = request.POST['password']  # 从表单中获取密码并添加干扰值
+        md5.update(s.encode('utf-8'))  # 将要产生md5的子串放进去
+        user.password_hash = md5.hexdigest()  # 获取md5值
+        if user.status == 6:
+            pass
+        else:
+            context = {'info': '无效的登录账号'}
+        return render(request,'myadmin/user/index.html',context)
+    except Exception as e:
+        context= {'info':'账号不存在'}
+        print(e)
+    return render(request,'myadmin/index/login.html',context)
 
 # 管理员退出
 def logout(request):
