4e98f318e5fb004132723bf6dc93142bee68fb38
myrondong <1052707337@qq.com>
2022-06-11 10:18:24 +0800

    pytest

--- pytest/all.py
+++ pytest/all.py
@@ -4,4 +4,4 @@ import pytest
 if __name__ == '__main__':
     # pytest.main(['-vs','./testcase/test_login.py','--reruns=2','-n=3','--maxfail=2','-k=ao'])
     pytest.main()
-    os.system('allure generate ./temp -o ./report --clean')
+    os.system('allure generate ./testdemo/report -o ./report --clean')
